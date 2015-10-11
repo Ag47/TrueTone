e55d8d779e4a24f7747a299fea7ce251299b9489
SILVER PANG <secretsilver@ymail.com>
2015-10-11 17:57:03 +0800

    Update API 22

---
+++ app/build/intermediates/ndk/debug/obj/local/armeabi/objs/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct/Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.o.d
@@ -0,0 +1,56 @@
+/Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/build/intermediates/ndk/debug/obj/local/armeabi/objs/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct//Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.o: \
+ /Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.cpp \
+ /Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.h \
+ /Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/jni.h \
+ /Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/sys/cdefs.h \
+ /Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/android/api-level.h \
+ /Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/android/log.h \
+ /Applications/eclipse/android-ndk-macosx/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ /Applications/eclipse/android-ndk-macosx/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring \
+ /Applications/eclipse/android-ndk-macosx/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio \
+ /Applications/eclipse/android-ndk-macosx/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
+ /Applications/eclipse/android-ndk-macosx/sources/cxx-stl/gnu-libstdc++/4.8/include/sstream \
+ /Applications/eclipse/android-ndk-macosx/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream \
+ /Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/stdlib.h \
+ /Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/alloca.h \
+ /Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/strings.h \
+ /Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/memory.h \
+ /Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/math.h \
+ /Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/assert.h
+
+/Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.h:
+
+/Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/jni.h:
+
+/Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/sys/cdefs.h:
+
+/Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/android/api-level.h:
+
+/Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/android/log.h:
+
+/Applications/eclipse/android-ndk-macosx/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/Applications/eclipse/android-ndk-macosx/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring:
+
+/Applications/eclipse/android-ndk-macosx/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio:
+
+/Applications/eclipse/android-ndk-macosx/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
+
+/Applications/eclipse/android-ndk-macosx/sources/cxx-stl/gnu-libstdc++/4.8/include/sstream:
+
+/Applications/eclipse/android-ndk-macosx/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream:
+
+/Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/stdlib.h:
+
+/Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/alloca.h:
+
+/Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/strings.h:
+
+/Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/memory.h:
+
+/Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/math.h:
+
+/Applications/eclipse/android-ndk-macosx/platforms/android-21/arch-arm/usr/include/assert.h:
