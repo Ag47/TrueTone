bfb20737153478fe858db4b48820c220878da55c
SILVER PANG <secretsilver@ymail.com>
2017-05-11 22:54:36 +0800

    Updtaed buildTools 25.0.3

--- app/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct/Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.o.d
+++ app/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct/Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.o.d
@@ -1,12 +1,12 @@
 /Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct//Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.o: \
- /Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.cpp \
- /Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.h \
- /Applications/eclipse/android-sdk-macosx/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
- /Applications/eclipse/android-sdk-macosx/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
- /Applications/eclipse/android-sdk-macosx/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
- /Applications/eclipse/android-sdk-macosx/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
- /Applications/eclipse/android-sdk-macosx/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/sstream \
- /Applications/eclipse/android-sdk-macosx/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream
+  /Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.cpp \
+  /Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.h \
+  /Applications/eclipse/android-sdk-macosx/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+  /Applications/eclipse/android-sdk-macosx/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+  /Applications/eclipse/android-sdk-macosx/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
+  /Applications/eclipse/android-sdk-macosx/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+  /Applications/eclipse/android-sdk-macosx/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/sstream \
+  /Applications/eclipse/android-sdk-macosx/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream
 
 /Users/SILVER/Documents/CCCU/Year2/SemesterB/AST20613CSD/FYP/TrueTone/app/src/main/jni/com_comp4905_jasonfleischer_midimusic_audio_NDKFunct.h:
 
